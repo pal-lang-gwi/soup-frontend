17cc81b45d68fab20af562c9436c8450f18b03e6
김여준 <kt65346337@gmail.com>
2025-06-12 12:41:35 +0900

    🛠️fix : 콘솔 제거 및 에러 메시지 반환 구조 수정

--- src/api/user/user.ts
+++ src/api/user/user.ts
@@ -5,12 +5,11 @@ import { AxiosError } from "axios";
 export const validateNickname = async (params: {nickname?: string}): Promise<boolean | null> => {
     try {
         const response = await api.get('/users/check-nickname', { params });
-        console.log(response);
         return response.data.data;
     } catch (error) {
         //백엔드에서 보내주는 에러메시지를 던집니다
         const err = error as AxiosError<{ error: { message: string } }>;
-        const message = err.response?.data?.error?.message;
+        const message = err.response?.data?.error?.message ?? 'Unknown Error';
         throw new Error(message);
     }
 }
