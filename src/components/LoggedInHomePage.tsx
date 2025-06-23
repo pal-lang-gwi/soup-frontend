d43b8f52c692204b0b9995e4d164d764c257fd37
유재광 <marmong9770@gmail.com>
2025-06-23 21:30:28 +0900

    feat: 홈 기능 소개 섹션을 전체 화면 스크롤 구조로 리뉴얼

--- src/components/LoggedInHomePage.tsx
+++ src/components/LoggedInHomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { searchKeywords } from "../api/keywords";
 import { searchKeywordDto } from "../types/keyword";
